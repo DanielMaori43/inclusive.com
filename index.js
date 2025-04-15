be430b9a32f524b4f4a062510c5baac0c63272e2
DanielMaori43 <projetositeviagens@gmail.com>
2025-04-15 16:14:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public'))); // onde estão seus HTML/JS/CSS
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
 var copiar = document.getElementById('copiar')
 
 
