bfc8f15cab3c6cc0119327a29531b50f5e4aad48
DanielMaori43 <projetositeviagens@gmail.com>
2025-04-15 20:05:52 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'Public'))); // onde estão seus HTML/JS/CSS
+app.use(express.static(path.join(__dirname, 'public'))); // onde estão seus HTML/JS/CSS
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
